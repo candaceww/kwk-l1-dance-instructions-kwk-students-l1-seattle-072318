6e9763217743ba27d8c50661ba715cf42f9f407f
candaceww <candacew224@gmail.com>
2018-07-24 16:58:25 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,31 @@
 
 # Code your methods below
+def Assume_Proper_Stance
+  puts "Assume the Proper Stance. 1. Plant legs far apart, bend knees slightly and keep posture loose"
+end
+def Base_Footwork
+  puts "1. Left right foot"
+  puts "2. Return right foot to the ground"
+  puts "3. Finishing with a small skip-step backward"
+def Skip_Step
+  puts "Lower left foot to the ground"
+  puts "Bound left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up skightly kicking it a few inches back"
+  puts "Right, Left, Right, Right"
+def Bob_The_Reins
+  puts "1. Holding your arms out in front of you, straight and at chest level"
+  puts "2. Cross your right wrist over your left and hold them together"
+  puts "3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+def Lasso
+  puts "1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "2. Lift right arm up and point right elbow diagonally to the right."
+  puts "3. Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+  def Bust_A_Move
+    puts "Bust a Move"
+    puts "-Be cool"
+    puts "-Be snazzy!"
+  puts Assume_Proper_Stance
+  puts Bust_A_Move
+  puts Base_Footwork
